acf6886911406299ee7f7db8270acfc9e26ebd38
Rosmaini Dollah <rosmainidollah79@gmail.com>
2015-10-20 05:49:54 +0800

    Update Webscrapping
    Updated comment on line 1-9

--- WebScrapping.java
+++ WebScrapping.java
@@ -1,8 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Assignment 2 - WebScrapping using java
+ * Information needed:  Webscrapping staff directory fksm.uitm.edu.my
  */
+
+/* Author : Rosmaini Dollah
+* ID : 2014845352
+* Github ID: rosmaini2014845352
+
+*/
 package webscrapping;
 
 
